58aa50288d52d7b99bf96d3b37554bcb04f80748
Ramon Lopes <dev.ramondasilvalopes@gmail.com>
2024-10-17 13:33:25 -0300

    inclusao de uma div

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,6 +26,8 @@ export default function Home() {
 
           <p>coloquei um paragrafo com mais uma alteração</p>
 
+          <div>inclusao de uma ddiv sem paragrafo</div>
+
       </main>
     </>
   );
